8bf40dabf5dbb5c4ccc47f2d92348b43edfde377
Raman Yasel <raman.yasel@gmail.com>
2023-03-03 16:34:29 +0300

    fix: tablet content height and add google-tag-manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,14 +14,18 @@ const RootLayout = ({ children }) => (
     <html lang="en" className="h-full">
       <head>
         {process.env.NODE_ENV === 'production' && (
-          <Script id="google-tag-manager" strategy="afterInteractive" />
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'';f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer', 'GTM-MJLTK6F');
+      `}
+          </Script>
         )}
       </head>
       <body>
         {process.env.NODE_ENV === 'production' && (
           <noscript
             dangerouslySetInnerHTML={{
-              __html: ``,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJLTK6F" height="0" width="0" style="display: none; visibility: hidden" aria-hidden="true"></iframe>`,
             }}
           />
         )}
