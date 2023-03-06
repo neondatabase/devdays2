130c56e3270b1d279db9bbc032906637340b4414
Raman Yasel <raman.yasel@gmail.com>
2023-03-06 17:15:09 +0300

    fix: add preload for blank ticket

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -17,6 +17,27 @@ const Head = () => (
       href="/images/developer-days/elephant-depth.jpg"
       as="image"
     />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-large.png"
+      as="image"
+      media="(min-width: 1024px)"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-medium.png"
+      as="image"
+      media="(min-width: 768px) and (max-width: 1023px)"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-small.png"
+      as="image"
+      media="(max-width: 767px)"
+    />
   </>
 );
 
