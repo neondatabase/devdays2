d4f84af3183fcbc49542ee95c186b983f1869f57
Thi Van Le <vannnyle@gmail.com>
2023-10-09 18:07:33 +0200

    feat: add animation preload

--- src/app/agenda/page.jsx
+++ src/app/agenda/page.jsx
@@ -8,6 +8,40 @@ export const metadata = getMetadata(SEO_DATA.agenda);
 
 const AgendaPage = () => (
   <Layout>
+    <link rel="preload" crossOrigin="anonymous" href="/animations/input-lines.riv" as="fetch" />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-defuse.jpg"
+      as="image"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-depth.jpg"
+      as="image"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-large.png"
+      as="image"
+      media="(min-width: 1024px)"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-medium.png"
+      as="image"
+      media="(min-width: 768px) and (max-width: 1023px)"
+    />
+    <link
+      rel="preload"
+      crossOrigin="anonymous"
+      href="/images/developer-days/elephant-0-small.png"
+      as="image"
+      media="(max-width: 767px)"
+    />
     <Hero />
     <AgendaTable />
   </Layout>
