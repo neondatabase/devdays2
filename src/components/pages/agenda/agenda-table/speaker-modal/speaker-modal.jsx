5ab9873ef4434d666982057cb70bfdda92b52520
Thi Van Le <vannnyle@gmail.com>
2023-10-11 17:50:34 +0200

    fix: remove overflow-hidden

--- src/components/pages/agenda/agenda-table/speaker-modal/speaker-modal.jsx
+++ src/components/pages/agenda/agenda-table/speaker-modal/speaker-modal.jsx
@@ -29,7 +29,7 @@ const SpeakerModal = ({
     isOpen={isOpen}
     contentLabel={name}
     ariaHideApp={false}
-    bodyOpenClassName="overflow-hidden touch-none"
+    bodyOpenClassName="touch-none"
     className="relative top-1/2 left-1/2 max-h-[95%] w-[95%] max-w-[458px] -translate-x-1/2 -translate-y-1/2 rounded-[10px] bg-black text-white shadow-[0px_14px_56px_0px_rgba(0,229,153,0.12),0px_6px_10px_0px_rgba(0,0,0,0.25)] border border-gray-15 after:bg-[linear-gradient(245deg,rgba(0,229,153,0.08)0%,rgba(0,229,153,0.5)100%)] after:absolute after:-inset-px after:rounded-[10px] after:p-px focus:outline-none"
     shouldCloseOnOverlayClick
     onRequestClose={closeModal}
@@ -99,7 +99,7 @@ const SpeakerModal = ({
 );
 
 SpeakerModal.propTypes = {
-  avatar: PropTypes.string.isRequired,
+  avatar: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   role: PropTypes.string.isRequired,
   bio: PropTypes.string,
