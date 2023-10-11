3bbff46235957d9f271c32b77dad3daccd6b3add
Thi Van Le <vannnyle@gmail.com>
2023-10-11 12:04:27 +0200

    fix: separate speaker modal

--- src/components/pages/agenda/agenda-table/agenda-table.jsx
+++ src/components/pages/agenda/agenda-table/agenda-table.jsx
@@ -1,48 +1,70 @@
+'use client';
+
+import { useState } from 'react';
+
 import { DEV_DAYS_AGENDA } from 'constants/dev-days';
 import patternSvg from 'images/agenda/pattern.svg';
 
 import Speaker from './speaker';
+import SpeakerModal from './speaker-modal';
 
-const AgendaTable = () => (
-  <section className="relative mt-14 mb-[105px] sm:mb-24">
-    <img
-      className="absolute right-0 bottom-[183px] [@media(max-width:1440px)]:hidden"
-      src={patternSvg}
-      width={378}
-      height={768}
-      alt=""
-      loading="lazy"
-      aria-hidden
-    />
-    <div className="relative mx-auto grid max-w-[1760px] w-full flex-grow grid-cols-12 gap-10 2xl:px-14 xl:grid-cols-1 xl:gap-0 xl:px-11 xl:py-11 lg:px-8 lg:py-9 md:px-4 md:py-4 text-white sm:py-0">
-      <div className="col-start-2 col-span-10 max-w-[1240px] 2xl:col-start-1 2xl:col-span-full lg:overflow-x-auto lg:max-w-none lg:-mx-8 md:-mx-4 lg:px-8 md:px-4 sm:overflow-visible sm:px-0 sm:mx-0">
-        <table className="w-full lg:min-w-[680px] sm:min-w-fit">
-          <thead className="text-left text-primary-4 text-sm font-medium leading-none border-b border-gray-15 sm:hidden">
-            <tr>
-              <th className="tracking-[0.02em] pb-5 pr-4">Speaker</th>
-              <th className="tracking-[0.02em] pb-5 pr-4">Talk</th>
-              <th className="tracking-[0.02em] pb-5">Company</th>
-            </tr>
-          </thead>
-          <tbody className="text-lg leading-dense divide-y divide-gray-15 border-b border-gray-15 md:text-base">
-            {DEV_DAYS_AGENDA.map(({ event, company, speaker }, index) => (
-              <tr className="sm:flex sm:flex-col-reverse sm:py-4 sm:first:pt-0" key={index}>
-                <td className="py-4 pr-4 sm:pr-0 sm:py-0 sm:mt-2.5">
-                  <Speaker {...speaker} company={company} />
-                </td>
-                <td className="tracking-[-0.02em] py-4 text-xl pr-4 lg:text-base sm:text-xl sm:pr-0 sm:py-0 sm:leading-[1.25] xs:text-lg">
-                  {event}
-                </td>
-                <td className="tracking-[-0.02em] py-4 font-semibold text-gray-80 sm:hidden">
-                  {company}
-                </td>
+const AgendaTable = () => {
+  const [selectedSpeaker, setSelectedSpeaker] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const closeModal = (e) => {
+    e.stopPropagation();
+    setShowModal(false);
+  };
+
+  return (
+    <section className="relative mt-14 mb-[105px] sm:mb-24">
+      <img
+        className="absolute right-0 bottom-[183px] [@media(max-width:1440px)]:hidden"
+        src={patternSvg}
+        width={378}
+        height={768}
+        alt=""
+        loading="lazy"
+        aria-hidden
+      />
+      <div className="relative mx-auto grid max-w-[1760px] w-full flex-grow grid-cols-12 gap-10 2xl:px-14 xl:grid-cols-1 xl:gap-0 xl:px-11 xl:py-11 lg:px-8 lg:py-9 md:px-4 md:py-4 text-white sm:py-0">
+        <div className="col-start-2 col-span-10 max-w-[1240px] 2xl:col-start-1 2xl:col-span-full lg:overflow-x-auto lg:max-w-none lg:-mx-8 md:-mx-4 lg:px-8 md:px-4 sm:overflow-visible sm:px-0 sm:mx-0">
+          <table className="w-full lg:min-w-[680px] sm:min-w-fit">
+            <thead className="text-left text-primary-4 text-sm font-medium leading-none border-b border-gray-15 sm:hidden">
+              <tr>
+                <th className="tracking-[0.02em] pb-5 pr-4">Speaker</th>
+                <th className="tracking-[0.02em] pb-5 pr-4">Talk</th>
+                <th className="tracking-[0.02em] pb-5">Company</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="text-lg leading-dense divide-y divide-gray-15 border-b border-gray-15 md:text-base">
+              {DEV_DAYS_AGENDA.map(({ event, company, speaker }, index) => (
+                <tr className="sm:flex sm:flex-col-reverse sm:py-4 sm:first:pt-0" key={index}>
+                  <td className="py-4 pr-4 sm:pr-0 sm:py-0 sm:mt-2.5">
+                    <Speaker
+                      speaker={speaker}
+                      company={company}
+                      setShowModal={setShowModal}
+                      setSelectedSpeaker={setSelectedSpeaker}
+                    />
+                  </td>
+                  <td className="tracking-[-0.02em] py-4 text-xl pr-4 lg:text-base sm:text-xl sm:pr-0 sm:py-0 sm:leading-[1.25] xs:text-lg">
+                    {event}
+                  </td>
+                  <td className="tracking-[-0.02em] py-4 font-semibold text-gray-80 sm:hidden">
+                    {company}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  </section>
-);
+      {selectedSpeaker?.bio && (
+        <SpeakerModal {...selectedSpeaker} isOpen={showModal} closeModal={closeModal} />
+      )}
+    </section>
+  );
+};
 
 export default AgendaTable;
--- src/components/pages/agenda/agenda-table/speaker/speaker.jsx
+++ src/components/pages/agenda/agenda-table/speaker/speaker.jsx
@@ -1,159 +1,64 @@
-'use client';
-
 import clsx from 'clsx';
 import Image from 'next/image';
-import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import ReactModal from 'react-modal';
 
-import CloseIcon from 'icons/close-gradient.inline.svg';
-import GithubIcon from 'icons/github.inline.svg';
-import LinkedinIcon from 'icons/linkedin-logo.inline.svg';
-import XIcon from 'icons/x-logo.inline.svg';
-
-const Speaker = ({
-  company,
-  avatar,
-  name,
-  role,
-  bio = null,
-  xUrl = null,
-  linkedinUrl = null,
-  githubUrl = null,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModal = () => setIsOpen(true);
-
-  const closeModal = (e) => {
-    e.stopPropagation();
-    setIsOpen(false);
+const Speaker = ({ speaker, company, setShowModal, setSelectedSpeaker }) => {
+  const { avatar, name, role, bio } = speaker;
+  const openModal = () => {
+    setShowModal(true);
+    setSelectedSpeaker({ ...speaker, company });
   };
+
   return (
-    <>
-      <button
-        className={clsx(
-          'flex items-center gap-x-2.5 group sm:w-full',
-          bio ? 'cursor-pointer' : 'cursor-auto'
-        )}
-        type="button"
-        onClick={openModal}
-      >
-        {avatar && (
-          <Image
-            className="rounded-full sm:w-8 sm:h-8"
-            src={avatar}
-            alt={name}
-            width={40}
-            height={40}
-          />
-        )}
-        <div className="flex flex-col items-start text-left">
-          <span
-            className={clsx(
-              'text-base tracking-[-0.02em] font-medium duration-200 transition-colors sm:text-[15px]',
-              { 'group-hover:text-primary-4': bio }
-            )}
-          >
-            {name}
-          </span>
-          {role && (
-            <span className="mt-0.5 text-[13px] text-gray-80 font-light">
-              <span className="tracking-[-0.02em]">{role}</span>
-              <span className="hidden sm:inline" aria-hidden>
-                , {company}
-              </span>
-            </span>
-          )}
-        </div>
-      </button>
-      {bio && (
-        <ReactModal
-          style={{
-            overlay: {
-              backgroundColor: 'rgba(0, 0, 0, 0.4)',
-              zIndex: '30',
-            },
-          }}
-          isOpen={isOpen}
-          contentLabel={name}
-          ariaHideApp={false}
-          bodyOpenClassName="overflow-hidden touch-none"
-          className="relative top-1/2 left-1/2 max-h-[95%] w-[95%] max-w-[458px] -translate-x-1/2 -translate-y-1/2 rounded-[10px] bg-black text-white shadow-[0px_14px_56px_0px_rgba(0,229,153,0.12),0px_6px_10px_0px_rgba(0,0,0,0.25)] border border-gray-15 after:bg-[linear-gradient(245deg,rgba(0,229,153,0.08)0%,rgba(0,229,153,0.5)100%)] after:absolute after:-inset-px after:rounded-[10px] after:p-px"
-          closeTimeoutMS={200}
-          shouldCloseOnOverlayClick
-          onRequestClose={closeModal}
-        >
-          <div className="relative z-10 bg-black px-6 pt-6 pb-7 rounded-[10px]">
-            <button className="absolute top-7 right-6" type="button" onClick={closeModal}>
-              <CloseIcon className="w-6 h-6" />
-            </button>
-            <div className="flex items-center gap-x-2.5">
-              {avatar && (
-                <Image
-                  className="rounded-full sm:w-8 sm:h-8"
-                  src={avatar}
-                  alt={name}
-                  width={40}
-                  height={40}
-                />
-              )}
-              <div className="flex flex-col items-start leading-dense">
-                <span className="text-base tracking-[-0.02em] font-medium sm:text-[15px]">
-                  {name}
-                </span>
-                {role && (
-                  <span className="mt-0.5 text-[13px] tracking-[-0.02em] text-gray-80 font-light">
-                    {role}
-                  </span>
-                )}
-              </div>
-            </div>
-            <div className="mt-[18px] pt-[18px] border-t border-gray-15 font-light text-sm leading-[1.25] text-gray-90">
-              {bio}
-            </div>
-            {(xUrl || linkedinUrl || githubUrl) && (
-              <div className="text-white mt-[18px] flex gap-x-5">
-                {xUrl && (
-                  <Link className="hover:text-primary-4 transition-colors duration-200" href={xUrl}>
-                    <XIcon className="w-4 h-4" />
-                  </Link>
-                )}
-                {linkedinUrl && (
-                  <Link
-                    className="hover:text-primary-4 transition-colors duration-200"
-                    href={linkedinUrl}
-                  >
-                    <LinkedinIcon className="w-4 h-4" />
-                  </Link>
-                )}
-                {githubUrl && (
-                  <Link
-                    className="hover:text-primary-4 transition-colors duration-200"
-                    href={githubUrl}
-                  >
-                    <GithubIcon className="w-4 h-4" />
-                  </Link>
-                )}
-              </div>
-            )}
-          </div>
-        </ReactModal>
+    <button
+      className={clsx(
+        'flex items-center gap-x-2.5 group sm:w-full',
+        bio ? 'cursor-pointer' : 'cursor-auto'
       )}
-    </>
+      type="button"
+      onClick={openModal}
+    >
+      {avatar && (
+        <Image
+          className="rounded-full sm:w-8 sm:h-8"
+          src={avatar}
+          alt={name}
+          width={40}
+          height={40}
+        />
+      )}
+      <div className="flex flex-col items-start text-left">
+        <span
+          className={clsx(
+            'text-base tracking-[-0.02em] font-medium duration-200 transition-colors sm:text-[15px]',
+            { 'group-hover:text-primary-4': bio }
+          )}
+        >
+          {name}
+        </span>
+        {role && (
+          <span className="mt-0.5 text-[13px] text-gray-80 font-light">
+            <span className="tracking-[-0.02em]">{role}</span>
+            <span className="hidden sm:inline" aria-hidden>
+              , {company}
+            </span>
+          </span>
+        )}
+      </div>
+    </button>
   );
 };
 
 Speaker.propTypes = {
+  speaker: PropTypes.shape({
+    avatar: PropTypes.object,
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string,
+    bio: PropTypes.string,
+  }).isRequired,
   company: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  role: PropTypes.string.isRequired,
-  bio: PropTypes.string,
-  xUrl: PropTypes.string,
-  linkedinUrl: PropTypes.string,
-  githubUrl: PropTypes.string,
+  setShowModal: PropTypes.func.isRequired,
+  setSelectedSpeaker: PropTypes.func.isRequired,
 };
 
 export default Speaker;
